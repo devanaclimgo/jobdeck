3551439ea500665d51adcf080653e748a2f11782
Ana <anaclimgo@gmail.com>
2026-06-13 17:32:55 -0300

    feat: simplify before_action condition in ApplicationController

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
-  before_action :configure_permitted_parameters,
-                if: :devise_controller?
+  before_action if: :devise_controller?
 
   protected
 end
